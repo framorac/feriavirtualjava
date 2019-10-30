f080d5fac3d632994139366012af48d24720e081
Francisco Mora <francisco.m.collao@gmail.com>
2019-10-30 19:07:02 -0300

    Initial commit

---
+++ src/main/java/duoc/portafolio/feriavirtual/FeriavirtualApplication.java
@@ -0,0 +1,13 @@
+package duoc.portafolio.feriavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriavirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriavirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duoc/portafolio/feriavirtual/FeriavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package duoc.portafolio.feriavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
