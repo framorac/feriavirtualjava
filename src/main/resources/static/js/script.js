22106de02862d51763d9484d858ef4f813734469
Francisco Mora <francisco.m.collao@gmail.com>
2019-11-04 20:38:36 -0300

    Se finaliza view login.html

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,4 +4,27 @@
  */
 $(function () {
    console.log('Ready....');
+   
+   $('.ui.form').form({
+     fields: {
+       email: {
+         identifier  : 'username',
+         rules: [
+           {
+             type   : 'empty',
+             prompt : 'Usuario es obligatorio..'
+           }
+         ]
+       },
+       password: {
+         identifier  : 'password',
+         rules: [
+           {
+             type   : 'empty',
+             prompt : 'Password obligatoria..'
+           }
+         ]
+       }
+     }
+   });
 });
