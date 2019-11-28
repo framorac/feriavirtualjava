781b604fcb6052b13ea6689ae79bac6379dcf916
Francisco Mora <francisco.m.collao@gmail.com>
2019-11-28 20:48:07 -0300

    Se agrega service para ofertaService y DetalleOfertaService.

---
+++ src/main/java/duoc/portafolio/feriavirtual/service/OfertaService.java
@@ -0,0 +1,8 @@
+package duoc.portafolio.feriavirtual.service;
+
+import duoc.portafolio.feriavirtual.commons.GenericService;
+import duoc.portafolio.feriavirtual.models.Oferta;
+
+public interface OfertaService extends GenericService<Oferta, Integer>{
+
+}
