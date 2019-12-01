1ff0eca4de10fdea0d295787c1831840d15ad0eb
Francisco Mora <francisco.m.collao@gmail.com>
2019-12-01 20:08:13 -0300

    Se completa reportes inicial.

--- src/main/java/duoc/portafolio/feriavirtual/controllers/ReporteController.java
+++ src/main/java/duoc/portafolio/feriavirtual/controllers/ReporteController.java
@@ -1,8 +1,32 @@
 package duoc.portafolio.feriavirtual.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import duoc.portafolio.feriavirtual.repository.SubastaRepository;
+import duoc.portafolio.feriavirtual.repository.UsuarioRepository;
+import duoc.portafolio.feriavirtual.repository.VentaRepository;
 
 @Controller
 public class ReporteController {
-
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private VentaRepository ventaRepository;
+	
+	@Autowired
+	private SubastaRepository subastaRepository;
+	
+	@RequestMapping("/reportes")
+	public String index(Model modelo) {
+		modelo.addAttribute("usuarios", usuarioRepository.count());
+		modelo.addAttribute("ventas", ventaRepository.count());
+		modelo.addAttribute("subastas", subastaRepository.count());
+		
+		return "reportes/reportes";
+	}
 }
