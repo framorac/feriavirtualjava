f8950ef61e4e1129c00d73bfbdd124ab0317360f
Francisco Mora <francisco.m.collao@gmail.com>
2019-12-01 18:58:53 -0300

    Cambios en agregar venta.

---
+++ src/main/java/duoc/portafolio/feriavirtual/service/impl/DetalleVentaServiceImpl.java
@@ -0,0 +1,20 @@
+package duoc.portafolio.feriavirtual.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+
+import duoc.portafolio.feriavirtual.commons.GenericServiceImpl;
+import duoc.portafolio.feriavirtual.models.DetalleVenta;
+import duoc.portafolio.feriavirtual.repository.DetalleVentaRepository;
+import duoc.portafolio.feriavirtual.service.DetalleVentaService;
+
+public class DetalleVentaServiceImpl extends GenericServiceImpl<DetalleVenta, Integer> implements DetalleVentaService{
+
+	@Autowired
+	private DetalleVentaRepository detalleVentaRepository;
+	
+	@Override
+	public CrudRepository<DetalleVenta, Integer> getRepository() {
+		return detalleVentaRepository;
+	}
+}
